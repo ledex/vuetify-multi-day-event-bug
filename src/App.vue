5ea02d0c9df21593bfaa45145cd309cf79e51109
Karsten Bruhns <karstenbruhns@gmail.com>
2024-01-17 19:41:55 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-calendar :events="events"/>
+      The event does not span across the entire duration, but is present twice, on the start date and end date.
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+const events = [
+  {
+    title: `Green event`,
+    start: new Date('2024-01-10 00:00'),
+    end: new Date('2024-01-15 23:59'),
+    allDay: false,
+    color: 'green'
+  },
+]
+</script>
